c2b2f5d746bacbe6efb70aba3a55ad373e8762b2
Lucas William <lucaswilliamml@gmail.com>
2025-06-25 18:06:52 -0300

    Add Logo

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { LanguageSwitcher } from "./LanguageSwitcher";
 import { ModeToggle } from "./ModeToggle";
 import { useTranslation } from "react-i18next";
+import LOGO from "../assets/logo.png"
 
 export function Navbar() {
   const { t } = useTranslation();
@@ -8,7 +9,11 @@ export function Navbar() {
   return (
     <header className="w-full border-b border-border bg-background">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <h1 className="text-xl font-semibold">{t("navbar.title")}</h1>
+        <div className="logo-name flex items-center gap-4">
+          <img src={LOGO} alt="" className="h-10 w-auto"/>
+          <h1 className="text-xl font-semibold">{t("navbar.title")}</h1>
+        </div>
+
         <div className="flex gap-2 items-center">
           <LanguageSwitcher />
           <ModeToggle />
