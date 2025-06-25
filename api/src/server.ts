a21c4de2081f03d4fbbb7281720402eb4df39399
Lucas William <lucaswilliamml@gmail.com>
2025-06-25 17:08:12 -0300

    Add Cors for backend to allow frontend requests

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import jobRoute from "./routes/jobRoute";
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: 'https://resume-keyword-reviewer.vercel.app',
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use("/api", jobRoute);
