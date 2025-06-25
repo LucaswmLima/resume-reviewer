f300f448dbe41dc0d07e5dde21b99f3b6ca9e559
Lucas William <lucaswilliamml@gmail.com>
2025-06-25 16:10:03 -0300

    Update add api base url to env and changed api code to fetch base_url

--- client/src/api/resumeAnalysisApi.ts
+++ client/src/api/resumeAnalysisApi.ts
@@ -1,11 +1,12 @@
 import type { ResumeAnalysisResult } from '../types/resumeAnalysisTypes';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const analyzeResumeTexts = async (
   jobText: string,
   cvText: string
 ): Promise<ResumeAnalysisResult> => {
-  const response = await fetch("/api/analyze", {
+  const response = await fetch(`${API_BASE_URL}/api/analyze`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
